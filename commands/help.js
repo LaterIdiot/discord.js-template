b606360a13809986ad480f210975756020d60ac2
LaterIdiot <52083057+Nayan-Bhut@users.noreply.github.com>
2021-01-12 11:35:29 -0500

    Changed formats

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,4 @@
-const {
-	prefix
-} = require('../index.js');
+const { prefix } = require("../index.js");
 
 module.exports = {
 	name: 'help',
@@ -10,18 +8,14 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args) {
 		const data = [];
-		const {
-			commands
-		} = message.client;
+		const { commands } = message.client;
 
 		if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
 			data.push(commands.map(command => command.name).join(', '));
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-			return message.author.send(data, {
-					split: true
-				})
+			return message.author.send(data, { split: true })
 				.then(() => {
 					if (message.channel.type === 'dm') return;
 					message.reply('I\'ve sent you a DM with all my commands!');
@@ -47,8 +41,6 @@ module.exports = {
 
 		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
-		message.channel.send(data, {
-			split: true
-		});
+		message.channel.send(data, { split: true });
 	},
 };
